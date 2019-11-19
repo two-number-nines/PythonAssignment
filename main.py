e05cafed9e6c09691428413aac7cfc08611a49f8
Vincent Mulder <vmulder@f0r3s19.codam.nl>
2019-11-19 15:15:35 +0100

    opening the zipfile and sending it one file at a time to convert_xml_to_csv

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import zipfile
+from bs4 import BeautifulSoup as Soup
+
+
+def convert_xml_to_csv(xml_file):
+    soup = Soup(xml_file, 'xml')
+    for batch in soup.findAll('xb:producten'):
+        print(batch)
 
 
 def main():
     if zipfile.is_zipfile('theZipFile.zip'):
         print('True')
         zf = zipfile.ZipFile('theZipFile.zip', 'r')
-        print(zf.namelist())
+        for name in zf.namelist():
+            f = zf.open(name)
+            #i open every xml file here
+            convert_xml_to_csv(f)
     else:
         print('provide a .zip file')
 
