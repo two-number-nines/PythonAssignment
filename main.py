69fb743008d4a348a23df773823038f6e2694aff
Vincent Mulder <vmulder@f1r6s12.codam.nl>
2019-11-20 15:42:28 +0100

    writing to csv, only need to fix einddatum

--- main.py
+++ main.py
@@ -1,55 +1,37 @@
 import zipfile
 from bs4 import BeautifulSoup as Soup
 import csv
-import xml.etree.ElementTree as ET
 
 
 def convert_xml_to_csv(xml_file):
-    # tree = ET.parse(xml_file)
-    # root = tree.getroot()
-    # f = open('/Users/vmulder/PycharmProjects/Octo/file.csv', 'w')
-    # writer = csv.writer(f)
-    # head = ['identificatie', 'openbareRuimteType', 'openbareRuimteNaam', 'BegindatumTijdvakGeldigheid',
-    #       'EinddatumTijdVakGeldigheid']
-    # writer.writerow(head)
+    f = open('/Users/vmulder/PycharmProjects/Octo/file.csv', 'w')
+    writer = csv.writer(f, delimiter='|')
+    head = ['identificatie', 'openbareRuimteNaam', 'BegindatumTijdvakGeldigheid',
+            'EinddatumTijdVakGeldigheid', 'openbareRuimteNaam']
+    writer.writerow(head)
     soup = Soup(xml_file, 'xml')
-    for x in soup.findAll('product_LVC:LVC-product'):
-        for item in x.findAll():
-                print(item)
-                identification = item.find('identificatie')
-                print('nu komt het')
-                print(identification)
-                exit()
-
-        # row = []
-        # identification = time.find('identificatie').find('Name').text
-        # row.append(identification)
-        '''
-        task_name = time.find('Task').find('Name').text
-        row.append(task_name)
-        staff_name = time.find('Staff').find('Name').text
-        row.append(staff_name)
-        date = time.find('Date').text
-        row.append(date)
-        minutes = time.find('Minutes').text
-        row.append(minutes)
-        billable = time.find('Billable').text
-        row.append(billable)
-        '''
-        # writer.writerow(row)
-    # f.close()
-
-
-'''
-def convert_xml_to_csv(xml_file):
-    soup = Soup(xml_file, 'xml')
-    for batch in soup.findAll('xb:producten'):
-        for item in batch.findAll('product_LVC:LVC-product'):
-            for element in item.findAll(True):
-                if element.name == 'OpenbareRuimte':
-                    print(element.name)
+    row = []
+    for q in soup.find('product_LVC:LVC-product'):
+        for x in q:
+            if x.find_parent().name == 'OpenbareRuimte' and x.name == 'identificatie':
+                row.append(x.string)
+            elif x.name == 'openbareRuimteNaam':
+                row.append(x.string)
+            elif x.name == 'openbareRuimteType':
+                row.append(x.string)
+            if x.name == 'tijdvakgeldigheid':
+                for y in x:
+                    print(y)
+                if x.name == 'begindatumTijdvakGeldigheid':
+                    row.append(x.string)
+                    print(x)
+                if x.find_next_sibling() == 'einddatumTijdvakGeldigheid':
+                    print("hoi")
+                    row.append(x.string)
                     exit()
-'''
+        writer.writerow(row)
+        row = []
+    f.close()
 
 
 def main():
