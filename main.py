0399f47c05d4c0087d5c97f0b468f149604f3638
Vincent Mulder <vmulder@f1r6s12.codam.nl>
2019-11-21 13:39:46 +0100

    error handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup as Soup
-import zipfile
+import zipfile as zipf
 import csv
 import sys
 
@@ -19,34 +19,37 @@ def convert_xml_to_csv(xml_file, csv_file):
                 row.append(element.string)
             elif element.name == 'tijdvakgeldigheid':
                 for x in element:
-                    if x.name == 'begindatumTijdvakGeldigheid':
-                        row.append(y.string)
-                    if x.name == 'einddatumTijdvakGeldigheid':
-                        row.append(y.string)
+                    if x.name == 'begindatumTijdvakGeldigheid' or x.name == 'einddatumTijdvakGeldigheid':
+                        row.append(x.string)
         writer.writerow(row)
         row = []
 
 
-def main(zip_file):
-    csv_file = open('/Users/vmulder/PycharmProjects/Octo/file.csv', 'w')
-    if zipfile.is_zipfile(zip_file):
-        zf = zipfile.ZipFile('theZipFile.zip', 'r')
-        count = 1
-        for name in zf.namelist():
-            if count == 4:
-                exit()
-            f = zf.open(name)
-            convert_xml_to_csv(f, csv_file)
-            f.close()
-            print("done with file: " + str(count))
-            count += 1
-        csv_file.close()
+def check_zip_file(zip_file):
+    if zipf.is_zipfile(zip_file):
+        zf = zipf.ZipFile(zip_file, 'r')
+        return zf
     else:
         print('provide a .zip file')
+        exit()
+
+
+def main(zip_file):
+    csv_file = open('/Users/vmulder/PycharmProjects/Octo/file.csv', 'w')
+    zf = check_zip_file(zip_file)
+    count = 1
+    for name in zf.namelist():
+        f = zf.open(name)
+        if name.endswith('.xml'):
+            convert_xml_to_csv(f, csv_file)
+            f.close()
+            print("done with file: " + name)
+            count += 1
+    csv_file.close()
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        print('Add .zip file as argument.')
+        print('Add one .zip file as argument.')
